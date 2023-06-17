4208d1e9475639e7da4c853015193480f78a75c0
Christian Looff <clooff@liquidpixels.com>
2023-06-17 08:32:37 -0400

    Install Tokio package and establish http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    // match_info -> 
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
 fn main() {
     println!("Hello, world!");
 }
