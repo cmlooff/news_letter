838c87573873cb922da27db1c58114592b89d632
Christian Looff <clooff@liquidpixels.com>
2023-06-18 15:00:11 -0400

    Health check end point
    Implement a request handler to the end point /health_check. This handler returns an empty body, but should recieve http status 200. Use curl -v [your_endpoint_here] to test. This check is manual at the moment, so implement CI pipeline soon.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use actix_web::{web, App, HttpServer, Responder, HttpRequest};
+use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
 
+// Request Handler: Greet
 async fn greet(req: HttpRequest) -> impl Responder {
-    // match_info -> 
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 
-fn main() {
-    println!("Hello, world!");
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    // Application listens to a TCP socket 127.0.0.1:8000
+    // HttpServer handles all transport level concerns
+    // but now we have to handle client request -> App
+    HttpServer::new(|| {
+        // This is where the App logic resides (routing, middleware, etc)!
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/user/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
