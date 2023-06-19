2b0e51fcd90ce3e9ef03ddca5d15a78d86aa0b62
Christian Looff <clooff@liquidpixels.com>
2023-06-19 17:13:52 -0400

    Create health_check integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,28 @@
+// 'tokio::test' is the testing equivalent of 'tokio::main'
+// It also spares you from having to specify the #[test] attribute
 
-use news_letter::main;
+// To inspect code generated: 
+// cargo expand --test health_check <- name of test file 
+#[tokio::test]
+async fn health_check_works() {
+  // Arrange
+  spawn_app().await.expect("Failed to spawn our app. ");
+  // Need to bring in `reqwest` to perform HTTP requests against our app
+  let client = reqwest::Client::new();
 
-#[test]
-fn dummy_test() {
-  main()
+  // Act
+  let response = client
+    .get("http://127.0.0.1:8000/health_check")
+    .sent()
+    .await
+    .expect("Failed to execute request.");
+
+  // Assert
+  assert!(response.status().is_success());
+  assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background ~somehow~
+async fn spawn_app() -> Result<(), std::io::Error> {
+  todo!()
 }
