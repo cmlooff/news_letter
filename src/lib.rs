b8db61e2e5b7918861ac87e79414e8213d8652b6
Christian Looff <clooff@liquidpixels.com>
2023-06-19 16:56:13 -0400

    library of functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn health_check() -> HttpResponse {
+  HttpResponse::Ok().finish()
+}
+
+// Need to mark 'run' as public.
+// No longer a binary entrypoint, there we mark it as async
+// without having to use any proc-macro incantation.
+pub async fn run() -> Result<(), std::io::Error> {
+  HttpServer::new( || {
+    App::new()
+      .route("/health_check", web::get().to(health_check))
+  })
+  .bind("127.0.0.1:8000")?
+  .run()
+  .await
+}
