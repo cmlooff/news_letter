3ea0cbdb2102d35f3c427779651428cf0443d7d8
Christian Looff <clooff@liquidpixels.com>
2023-06-22 13:59:39 -0400

    Clean comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::net::TcpListener;
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind to random port");
-    // let port = listener.local_addr().unwrap().port();
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
