28a59ebbe5658d74b9cc90236ab6d9388573578d
Christian Looff <clooff@liquidpixels.com>
2023-07-03 09:58:40 -0400

    Add success test for post request 200.
    Using x-www-form-urlencoded encoding, we send a body matching to the header requirements and save the response. Then assert that the two values equal to each other

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,10 +21,11 @@ async fn health_check_works() {
   assert_eq!(Some(0), response.content_length());
 }
 
-// Launch our application in the background ~somehow~
+// Launch our application in the background
 fn spawn_app() -> String {
   let listener = TcpListener::bind("127.0.0.1:0")
     .expect("Failed to bind random port");
+
   // Retrieve the port assigned to us by the OS
   let port = listener.local_addr().unwrap().port();
   let server = run(listener).expect("Failed to bind address");
@@ -33,4 +34,24 @@ fn spawn_app() -> String {
 
   // Return the application address to the caller
   format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16()); // Asserting that response status is equal to 200
 }
