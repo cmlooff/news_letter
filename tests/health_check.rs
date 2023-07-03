547733a7d4a05d8ad9a6d6f81fc63ef70b837c22
Christian Looff <clooff@liquidpixels.com>
2023-07-03 10:23:34 -0400

    Add bad request (400) test case.
    Using a vector of tuples to add the invalid_body and error_message pairs to be tested.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -55,3 +55,34 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     // Assert
     assert_eq!(200, response.status().as_u16()); // Asserting that response status is equal to 200
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both the email and the name")
+    ];
+
+    for(invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customized error message on test failure
+            "The API did note fail with a 400 Bad Request when the payload for {}.", error_message
+        );
+    }
+}
