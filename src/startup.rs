c366de244aa18a1ee41635478529a58280c791b8
Christian Looff <clooff@liquidpixels.com>
2023-08-21 14:38:17 -0400

    Need to have a copy of our connection for each copy of App.
    Use web::Data from actix-web to wrap our connection in an atomic reference counted pointer.

--- src/startup.rs
+++ src/startup.rs
@@ -7,10 +7,14 @@ use sqlx::PgConnection;
 // No longer a binary entrypoint, there we mark it as async
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener, connection:PgConnection) -> Result<Server, std::io::Error> {
-  let server = HttpServer::new( || {
+  // Wrap connection in a smart pointer
+  let connection = web::Data::new(connection);
+
+  let server = HttpServer::new(move || {
     App::new()
       .route("/health_check", web::get().to(health_check))
       .route("/subscriptions", web::post().to(subscribe))
+      .app_data(connection.clone())
   })
     .listen(listener)?
     .run();
