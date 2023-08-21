5bd256baf036be11d5cd843e2908f9319bde9dc7
Christian Looff <clooff@liquidpixels.com>
2023-08-21 11:40:05 -0400

    Add configuration reader to read from configuration.yaml.
    Change main.rs to pull from configuration.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::net::TcpListener;
 use news_letter::startup::run;
+use news_letter::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
-
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
+    // Panic if we can't read configuration
+    let configuration= get_configuration().expect("Failed to read configuration");
+    // We have removed the hard-coded '8000' it's now coming from our settings!
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, HttpResponse};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
