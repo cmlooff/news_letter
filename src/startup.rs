8baf2aa65db39789bc4395e99a1e9e1716499efa
Christian Looff <clooff@liquidpixels.com>
2023-09-26 10:58:30 -0400

    Add Log middlerware using .wrap method on App

--- src/startup.rs
+++ src/startup.rs
@@ -10,10 +10,11 @@ use std::net::TcpListener;
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
   // Wrap connection in a smart pointer
-  let db_pool = web::Data::new(db_pool);
+  let db_pool = Data::new(db_pool);
 
   let server = HttpServer::new(move || {
     App::new()
+      .wrap(Logger::default())
       .route("/health_check", web::get().to(health_check))
       .route("/subscriptions", web::post().to(subscribe))
       .app_data(db_pool.clone())
